2950283212bf7ec32719509f4d333612e8a66e33
MarkTiukov <tyukov.mark@gmail.com>
2020-12-14 02:30:52 +0300

    Fixed tests

--- main_test.py
+++ main_test.py
@@ -33,29 +33,29 @@ class MyTestCase(unittest.TestCase):
     def testFindLongestPrefixForRegular(self):
         self.assertEqual(findLongestPrefixForRegular("acbac", "acb..bab.c.*.ab.ba.+.+*a."), 5)
 
-    # def test1(self):
-    #     self.assertEqual(findLongestPrefixForRegular("aaab", "a*b."), 4)
-    #
-    # def test2(self):
-    #     self.assertEqual(findLongestPrefixForRegular("abab", "ab.abc..+ab.."), 4)
-    #
-    # def test3(self):
-    #     self.assertEqual(findLongestPrefixForRegular("abc", "a*b*.ab+c+.b*."), 3)
-    #
-    # def test4(self):
-    #     self.assertEqual(findLongestPrefixForRegular("abc", "a*b*a*..cb+."), 3)
-    #
-    # def test5(self):
-    #     self.assertEqual(findLongestPrefixForRegular("abacb", "ab+c.aba.*.bac.+.+*"), 5)
-    #
-    # def test6(self):
-    #     self.assertEqual(findLongestPrefixForRegular("acbac", "acb..bab.c.*.ab.ba.+.+*a."), 5)
-    #
-    # def test7(self):
-    #     self.assertEqual(findLongestPrefixForRegular("bbaab", "bb1aa...."), 4)
-    #
-    # def test8(self):
-    #     self.assertEqual(findLongestPrefixForRegular("bbaab", "bb.b1+aa..."), 4)
+    def test1(self):
+        self.assertEqual(findLongestPrefixForRegular("aaab", "a*b."), 4)
+
+    def test2(self):
+        self.assertEqual(findLongestPrefixForRegular("abab", "ab.abc..+ab.."), 4)
+
+    def test3(self):
+        self.assertEqual(findLongestPrefixForRegular("abc", "a*b*.ab+c+.b*."), 3)
+
+    def test4(self):
+        self.assertEqual(findLongestPrefixForRegular("abc", "a*b*a*..cb+."), 3)
+
+    def test5(self):
+        self.assertEqual(findLongestPrefixForRegular("abacb", "ab+c.aba.*.bac.+.+*"), 5)
+
+    def test6(self):
+        self.assertEqual(findLongestPrefixForRegular("acbac", "acb..bab.c.*.ab.ba.+.+*a."), 5)
+
+    def test7(self):
+        self.assertEqual(findLongestPrefixForRegular("bbaab", "bb1aa...."), 4)
+
+    def test8(self):
+        self.assertEqual(findLongestPrefixForRegular("bbaab", "bb.b1+aa..."), 4)
 
     def testBadData1(self):
         self.assertEqual(findLongestPrefixForRegular("any word", "ab.+*...."), "ERROR")
